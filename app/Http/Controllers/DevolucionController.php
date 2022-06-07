24e23de0b71b62b5e7ade3084e21f188f0e97fae
fer <you@example.com>
2022-06-06 22:58:53 -0500

    Devoluciones

--- app/Http/Controllers/DevolucionController.php
+++ app/Http/Controllers/DevolucionController.php
@@ -9,6 +9,7 @@ class DevolucionController extends Controller
 {
     /**
      * Display a listing of the resource.
+     * 
      * @return \Illuminate\Http\Response
      */
     public function index()
