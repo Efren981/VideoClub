ffee35be305c428b23098a582fe11f05ae11b5c8
Julio Alejandro López Espinoza <raptor.67.jL@gmail.com>
2022-06-18 18:53:30 -0500

    faraon

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,26 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::resource("socios",\App\Http\Controllers\SociosController::class);
+
+Route::resource("registro_personas",\App\Http\Controllers\PersonaController::class);
+
+Route::resource("registro_director",\App\Http\Controllers\DirectorController::class);
+
+Route::resource("registro_genero",\App\Http\Controllers\GeneroController::class);
+
+Route::resource('cintas',\App\Http\Controllers\CintaController::class);
+
+Route::resource('peliculas',\App\Http\Controllers\PeliculaController::class);
+
+Route::resource("devoluciones",\App\Http\Controllers\DevolucionController::class);
+
+Route::resource('prestamos',\App\Http\Controllers\PrestamoController::class);
+
+Route::resource("actores",\App\Http\Controllers\ActorController::class);
+
+Route::resource("emp",\App\Http\Controllers\empleado::class);
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
