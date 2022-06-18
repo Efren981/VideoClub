3b2f0255e4d63f4bcddd14b287ae85a2d706e6fc
Manis <joseaustin.z9439@gmail.com>
2022-06-18 15:20:29 -0500

    node

--- database/migrations/2022_06_18_000106_create_personas_table.php
+++ database/migrations/2022_06_18_000106_create_personas_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('personas', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
         });
     }
 
