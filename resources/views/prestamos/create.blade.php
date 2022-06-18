23dba716594750da0f23d43d1306e2e9e5829973
felipe garcia <felipefenix05@gmail.com>
2022-06-18 17:17:28 -0500

    prestamos

--- resources/views/prestamos/create.blade.php
+++ resources/views/prestamos/create.blade.php
@@ -1,1 +1,69 @@
-<?php
+@extends('layouts.app')
+
+@section("prestamos")
+    active
+@endsection
+@section("content")
+    <div class="row d-flex justify-content-center">
+        <div class="col-6 ">
+            <div class="row">
+                <div class="card">
+                    <h1 class="card-title mt-4 mb-4 alert-primary text-center">Generar Prestamo</h1>
+                    <div class="row d-flex justify-content-center">
+                        <div class="col-6">
+                            <div class="row d-flex justify-content-center">
+                                <div class="col-12">
+                                    <form method="POST" action="{{url("prestamos")}}">
+                                        @csrf
+                                        <div class="form-floating mb-3">
+                                            <input type="date" class="form-control @error ('date') is-invalid @enderror" id="nombre" placeholder=".." name="date">
+                                            <label for="nombre">Fecha Prestamo</label>
+                                        </div>
+                                        @error('date')
+                                        <div class="invalid-feedback">{{$message}}</div>
+                                        @enderror
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control @error ('socio') is-invalid @enderror" id="ap1" placeholder=".." name="socio">
+                                            <label for="ap1">Socio</label>
+
+                                        </div>
+                                        @error('socio')
+                                        <div class="invalid-feedback">{{$message}}</div>
+                                        @enderror
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control @error ('pelicula') is-invalid @enderror" id="ap2"placeholder=".." name="pelicula">
+                                            <label for="ap2">Pelicula</label>
+                                        </div>
+                                        @error('pelicula')
+                                        <div class="invalid-feedback">{{$message}}</div>
+                                        @enderror
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control @error ('cinta') is-invalid @enderror" id="ap2"placeholder=".." name="cinta">
+                                            <label for="ap2">Cinta</label>
+                                        </div>
+                                        @error('cinta')
+                                        <div class="invalid-feedback">{{$message}}</div>
+                                        @enderror
+
+                                        <div class="row my-5 d-flex justify-content-center">
+                                            <div class="col-3"></div>
+                                            <div class="col-3">
+                                                <a href="{{url("prestamos")}}" class="btn btn-danger mb-4 text-white"><span class="icon-arrow-thin-left" data-toggle="tooltip" title="Regresar al menu principal">Cancelar</span></a>
+
+                                            </div>
+                                            <div class="col-3">
+                                                <button class="btn btn-primary" type="submit"><span class="icon-checkmark" data-toggle="tooltip" title="Agregar nueva publicacion">Guardar</span></button>
+                                            </div>
+                                            <div class="col-3"></div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
---
+++ resources/views/prestamos/index.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section("prestamos")
+    active
+@endsection
+@section("content")
+    <div class="container">
+        <div class="row" >
+            <div class="col" >
+                <h1 class="text-center mb-5">Prestamos</h1>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-8">
+                    <div class="card">
+                        <a
+                            href="{{url("prestamos/create")}}"
+                            class="btn btn-dark btn-sm mb-4 text-white"
+                            data-toggle="tooltip"
+                            title="Cargar nuevo socio">
+                    <span
+                        class="icon-pen">
+                        Generar Prestamo
+                    </span>
+                        </a>
+                        <table class="table card" style="background: #e1e1e8">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col" class=" justify-content-center text-center">Fecha</th>
+                                <th scope="col" class=" justify-content-center text-center">Socio</th>
+                                <th scope="col" class=" justify-content-center text-center">Pelicula</th>
+                                <th scope="col" class=" justify-content-center text-center">Cinta</th>
+                                <th colspan="2" scope="col" class=" justify-content-center text-center">Acciones</th>
+
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($datos as $prestamos)
+                                <tr>
+                                    <td>{{$loop->index+1}}</td>
+                                    <td>{{$prestamos->fecha}}</td>
+                                    <td>{{$prestamos->socio}}</td>
+                                    <td>{{$prestamos->pelicula}}</td>
+                                    <td>{{$prestamos->cintas}}</td>
+                                    <td scope="col"><a href="{{route("prestamos.edit",$prestamos->id)}}" class="btn btn-success btn-sm" data-toggle="tooltip" title="Editar difusion"><span class="icon-compose">Editar</span></a></td>
+                                    <td scope="col"><form action="{{route("prestamos.destroy",$prestamos->id)}}" method="post">
+                                            @csrf
+                                            @method('delete')
+                                            <button class="btn btn-danger btn-sm" type="submit" data-toggle="tooltip" title="Eliminar esta difusion"><span class="icon-bin">Eliminar</span></button>
+                                        </form></td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
