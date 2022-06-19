8b745c3958a788af4edb17c292d803171cc15f0b
Efren981 <efrenbarcenas05@gmail.com>
2022-06-19 13:24:25 -0500

    Peliculas

--- resources/views/peliculas/update.blade.php
+++ resources/views/peliculas/update.blade.php
@@ -8,23 +8,41 @@
         <h4>Editar Pelicula</h4>
         <div class="row">
             <div class="col-xl-12">
-                <form action="{{route('peliculas.update',$pelicula->id_pelicula)}}" method="post">
-                    @csrf
-                    @method('PUT')
-                    <div class="form-group">
-                        <label for="id_pelicula">id_pelicula</label>
-                        <input type="text" class="form-control" name="id_pelicula" required maxlength="50"value="{{$pelicula->id_pelicula}}">
-                    </div>
-                    <div class="form-group">
-                        <label for="titulo">titulo</label>
-                        <input type="text" class="form-control" name="titulo" required maxlength="50"value="{{$pelicula->titulo}}">
-                    </div>
-                    <div class="for-group">
-                        <input type="submit" class="btn btn-primary" value="guardar">
-                        <input type="reset" class="btn btn-default" value="cancelar">
-                        <a href="javascript:history.back()">ir al listado</a>
-                    </div>
-                </form>
+              <form method="POST" action="{{route("peliculas.update",$pelicula->id)}}">
+                  @csrf
+                  @method('PUT')
+                  <div class="row mx-5 d-flex justify-content-center">
+                      <div class="col-10">
+                          <div class="form-floating mb-3">
+                              <input type="text" class="form-control @error('idGenero') is-invalid @enderror" id="idGenero" placeholder=".." name="idGenero" value="{{$pelicula->idGenero}}">
+                              <label for="newname">Nuevo Genero</label>
+                              @error('idGenero')
+                              <div class="invalid-feedback">{{$message}}<div>
+                                      @enderror
+                          </div>
+                          <div class="form-floating mb-3">
+                              <input type="text" class="form-control @error('titulo') is-invalid @enderror" id="titulo" placeholder=".." name="titulo" value="{{$pelicula->titulo}}">
+                              <label for="titulo">Nuevo titulo</label>
+                              @error('titulo')
+                              <div class="invalid-feedback">{{$message}}<div>
+                              @enderror
+                          </div>
+                      </div>
+                  </div>
+                  <div class="row my-5 d-flex justify-content-center">
+                      <div class="col-3"></div>
+                      <div class="col-3">
+                          <a href="{{url("personas")}}" class="btn btn-danger mb-4 text-white" data-toggle="tooltip" title="Regresar el menu principal">Cancelar</a>
+
+                      </div>
+                      <div class="col-3">
+                          <button class="btn btn-primary" type="submit" data-toggle="tooltip" title="Guardar cambios">Guardar</button>
+                      </div>
+                      <div class="col-3"></div>
+                  </div>
+          </div>
+      </div>
+      </form>
             </div>
         </div>
     </div>
