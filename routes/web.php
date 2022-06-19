6a1fb87ddfc2a8beb9b741de939e376f66e4116a
Manis <joseaustin.z9439@gmail.com>
2022-06-18 19:06:11 -0500

    la_caga_el_efren

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,3 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::resource("personas",\App\Http\Controllers\PersonasController::class);
